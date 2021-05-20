45980ceb55771e491f7fa55e2d3476c9b1042789
Michael <Mlcorrieri@gmail.com>
2021-05-20 16:00:25 -0400

    done.

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,7 @@
 class Employee < ApplicationRecord
+    belongs_to :dog 
+
+    validates :alias, :presence => true, :uniqueness => true
+    validates :title, :presence => true, :uniqueness => true
+
 end
